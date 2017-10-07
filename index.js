7561e3703c5104dff66ea1034397cc6c23807b1f
DeqingQu <qu.deqing@gmail.com>
2017-10-07 08:58:56 -0700

    include validator lib

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = express();
 var path = require('path');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
 
 var index = require('./routes/index');
 var companies = require('./routes/companies')
@@ -16,6 +17,7 @@ app.set('view engine', 'pug');
 app.use(morgan('short'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressValidator());
 app.use(express.static(__dirname + "/public"));
 
 app.use('/', index);
