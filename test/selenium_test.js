7da7b347ad3b618e3c0b6e7b1b19c88fc546e521
DeqingQu <qu.deqing@gmail.com>
2017-10-11 16:05:45 -0700

    there is a bug to check whether new element is existed in list, foreach to check doesnt work when async

--- test/selenium_test.js
+++ test/selenium_test.js
@@ -17,12 +17,37 @@ test.after(function () {
 });
 
 test.describe("testing site with selenium", function () {
-  test.it("should access homepage", function (done) {
-    browser.get('http://localhost:3000/companies');
+      test.it("should access homepage", function (done) {
+            browser.get('http://localhost:3000/companies');
 
-    // findElement already throws an exception if element not found
-    var lnkToSee = browser.findElement(selenium.By.partialLinkText('Add company'));
-    lnkToSee.click();
-    done();
-  });
+            var lnkAddCompany = browser.findElement(selenium.By.partialLinkText('Add company'));
+            lnkAddCompany.click();
+            done();
+      });
+
+      test.it("should let us fill in the form", function (done) {
+          // taking us to the form, where we put in a random test string about Mario's favorite villain
+          browser.findElement(selenium.By.name("company_name")).sendKeys("Monster");
+          browser.findElement(selenium.By.name("company_category")).sendKeys("FMCG");
+          browser.findElement(selenium.By.name("save")).click();
+          done();
+      });
+
+      test.it("should take us to the list page, including our new element", function (done) {
+          // ok, we should now be back at the list page... can we find our new Bowser link?
+          // findElement already throws an exception if element not found
+          var isMonster = false;
+          browser.findElements(selenium.By.tagName("li")).then(function(elements){
+              elements.forEach(function (element) {
+                  element.getText().then(function(text){
+                      if (text.inclues("Monster")) {
+                          isMonster = true;
+                      }
+                  });
+              });
+              expect(isMonster).to.equal(true);
+          });
+
+          done();
+      });
 });
