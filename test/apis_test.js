89ca3d00568d5269bcb87c7d213cb3affbc2ce76
DeqingQu <qu.deqing@gmail.com>
2017-10-13 00:58:17 -0700

    add superagent api test

--- test/apis_test.js
+++ test/apis_test.js
@@ -1,15 +1,52 @@
 const chai = require('chai');
 const expect = chai.expect;
+const superagent = require('superagent')
 
-describe("sanity check", () => {
-    it("does math ok", (done) => {
-        expect(1 + 1).to.equal(2);
-        expect(1 + 1).not.to.equal(1);
-        done();
+var BASE_URL = 'http://localhost:3000/api';
+var exist_company_id = 5;
+var no_exist_company_id = 1;
+
+describe("test site with superagent", () => {
+    it("test GET /url/api/companies", (done) => {
+        superagent.get(BASE_URL + '/companies')
+            .end(function(err, res) {
+                expect(err).to.not.exist;
+                expect(res).to.exist;
+                expect(res.status).to.equal(200);
+                expect(res.text).to.exist;
+
+                var companies = JSON.parse(res.text);
+                expect(companies).to.be.an.instanceof(Array);
+                done();
+            });
     });
-    it("supports the usual JS objects", (done) => {
-        expect(typeof "OSU").to.equal("string");
-        expect("OSU".length).to.equal(3);
-        done();
+    it("test GET /url/api/companies/{id} with existed Id", (done) => {
+        superagent.get(BASE_URL + '/companies/' + exist_company_id)
+            .end(function(err, res) {
+                expect(err).to.not.exist;
+                expect(res).to.exist;
+                expect(res.status).to.equal(200);
+                expect(res.text).to.exist;
+
+                var company = JSON.parse(res.text);
+                expect(company).to.have.property('company_id');
+                expect(company).to.have.property('company_name');
+                expect(company).to.have.property('company_category');
+                done();
+            });
+    });
+
+    it("test GET /url/api/companies/{id} with non-existed Id", (done) => {
+        superagent.get(BASE_URL + '/companies/' + no_exist_company_id)
+            .end(function(err, res) {
+                expect(err).to.not.exist;
+                expect(res).to.exist;
+                expect(res.status).to.equal(200);
+                expect(res.text).to.exist;
+
+                var company = JSON.parse(res.text);
+                expect(company).to.be.an('object').that.is.empty;
+                done();
+            });
     });
 });
