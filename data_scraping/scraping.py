0fa028c03db63fa52f9017bd9b4dba756d656ab7
DeqingQu <qu.deqing@gmail.com>
2017-10-17 14:48:42 -0700

    organize the companies in an object array

--- data_scraping/scraping.py
+++ data_scraping/scraping.py
@@ -6,11 +6,14 @@ import json
 
 BASE_URL = "https://finance.google.com"
 
-SEARCH_URL = "https://en.wikipedia.org/"
-TARGET_URL = "https://en.wikipedia.org/wiki/Python_(programming_language)"
-
-ticker_symbol = "NASDAQ:TSLA"#"HKG:0966"
+ticker_symbol = "HKG:0966"
 related_companies = []
+
+
+class Company:
+    def __init__(self, name, symbol):
+        self.name = name
+        self.symbol = symbol
 
 
 #   retrieve data from URL
@@ -23,7 +26,9 @@ def retrieve(url: str):
     return soup
 
 
+#   find related companies from Google Financial
 def find_related_companies(symbol: str, base_url: str):
+    companies = []
     url = base_url + '/finance?q=' + symbol
     #   get the link to related companies
     soup = retrieve(url)
@@ -38,52 +43,17 @@ def find_related_companies(symbol: str, base_url: str):
             # print(script.text)
             index_start = script.text.find('google.finance.data')
             index_end = script.text.find('google.finance.data.numberFormat')
-            print("index_start {} and index end {}".format(index_start, index_end))
             result = script.text[index_start + len('google.finance.data = '):index_end-2]
             json_result = json.loads(result)
-            print(json_result['company']['related']['rows'])
+            for related_company in json_result['company']['related']['rows']:
+                company_name = related_company['values'][1]
+                company_symbol = "{}:{}".format(related_company['values'][8], related_company['values'][0])
+                if company_symbol != symbol:
+                    companies.append(Company(company_name, company_symbol))
+    return companies
 
 
-def filter_links(url: str, base_url: str, search_url: str):
-    soup = retrieve(url)
-    links_after_filter = {}
-    links_before_filter = soup.find_all('a')
-    for link in links_before_filter:
-        href_url = str(link.get('href'))
-        #   is it a relevant URL to the BASE url?
-        if href_url.startswith("/") and not href_url.startswith("//") and href_url.find('#') == -1:
-            href_url = base_url + href_url
-        if href_url.startswith(search_url):
-            if not href_url in links_after_filter.keys():
-                links_after_filter[href_url] = href_url
-    return links_after_filter
+related_companies = find_related_companies(ticker_symbol, BASE_URL)
+for company in related_companies:
+    print("name : {}, symbol : {}".format(company.name, company.symbol))
 
-
-def get_date(date_str: str):
-    start_index = len('This page was last edited on ')
-    date_str = date_str[start_index+1:]
-    com_index = date_str.find(',')
-    date_str = date_str[:com_index]
-    modify_date = datetime.strptime(date_str, '%d %B %Y')
-    return modify_date
-
-
-def get_links_this_month(links: dict):
-    href_count_in_a_month = 0
-    for link in links:
-        detail_soup = retrieve(link)
-        for node in detail_soup.find_all('li'):
-            if str(node.get('id')) == "footer-info-lastmod":
-                time_str = node.string
-                modified_date = get_date(time_str)
-                now = datetime.now()
-                if (now - modified_date).days < 30:
-                    href_count_in_a_month += 1
-                    # print('final result + %d' % href_count_in_a_month)
-    print("Result %d" % href_count_in_a_month)
-
-
-# links_filtered = filter_links(TARGET_URL, BASE_URL, SEARCH_URL)
-# get_links_this_month(links_filtered)
-
-find_related_companies(ticker_symbol, BASE_URL)
